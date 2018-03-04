2251ebb847f6d4eb7dd5e6560a93fbf8ff53efca
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-03-04 13:22:06 +0600

    Simple Web View Part-01

---
+++ app/src/main/java/com/example/mahadi/webview/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.mahadi.webview;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private ImageView imageView;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        imageView   = (ImageView) findViewById(R.id.icons);
+        webView     = (WebView)  findViewById(R.id.myWebView);
+
+
+        progressBar.setMax(100);
+        webView.loadUrl("https://www.google.com/");
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                getSupportActionBar().setTitle(title);
+
+            }
+
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                super.onReceivedIcon(view, icon);
+                imageView.setImageBitmap(icon);
+            }
+        });
+
+    }
+}
