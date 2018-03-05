85e87fc3c4f3704e05e8154c297325496e1974b9
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-03-05 10:27:59 +0600

    full menu completed

--- app/src/main/java/com/example/mahadi/webview/MainActivity.java
+++ app/src/main/java/com/example/mahadi/webview/MainActivity.java
@@ -4,12 +4,14 @@ import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,14 +60,40 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.super_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.menu_back:
+                onBackPressed();
+                break;
+
+            case R.id.menu_foreword:
+                onForwardProcess();
+                break;
+
+            case R.id.menu_refresh:
+                webView.reload();
+                break;
+        }
         return super.onOptionsItemSelected(item);
     }
 
+//    User Defined method
+    private void onForwardProcess(){
+
+        if (webView.canGoForward())
+            webView.goForward();
+        else
+            Toast.makeText(this,"Can't go further !",Toast.LENGTH_LONG).show();
+    }
     @Override
     public void onBackPressed() {
         if (webView.canGoBack())
